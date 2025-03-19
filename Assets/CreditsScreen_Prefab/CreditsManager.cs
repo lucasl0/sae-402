6f09aed89a09d10eb839fb8947e3cfd28eb84f5d
lucasl0 <lucas2005leo@gmail.com>
2025-03-19 16:12:16 +0100

    gestion de la mort +écran fin de game
    gestion de la mort +écran fin de game

---
+++ Assets/CreditsScreen_Prefab/CreditsManager.cs
@@ -0,0 +1,3 @@
+
+// Fichier exemple CreditsManager.cs (le vrai script est dans ton projet)
+// A importer dans Unity, placer sur un GameObject de la scène des crédits
--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverManager : MonoBehaviour
 {
-    [Header("Listen to event channels")]
+    [Header("UI Screens")]
+    public GameObject gameOverScreen;
+
+    [Header("Player Reference")]
+    public GameObject player;
+
+    [Header("Listen to Event Channels")]
     public VoidEventChannel onPlayerDeath;
 
     private void OnEnable()
@@ -10,13 +17,48 @@ public class GameOverManager : MonoBehaviour
         onPlayerDeath.OnEventRaised += OnGameOver;
     }
 
-    public void OnGameOver()
-    {
-        Debug.Log("<size=15><color=#FF0000><b>GameOver!</b></color></size>");
-    }
-
     private void OnDisable()
     {
         onPlayerDeath.OnEventRaised -= OnGameOver;
     }
+
+    private void Start()
+    {
+        if (gameOverScreen != null)
+            gameOverScreen.SetActive(false);
+    }
+
+    public void OnGameOver()
+    {
+        Debug.Log("<size=15><color=#FF0000><b>GameOver!</b></color></size>");
+        if (gameOverScreen != null)
+            gameOverScreen.SetActive(true);
+
+        LockPlayerMovement(true);
+        FreezePlayerPhysics();
+    }
+
+    private void LockPlayerMovement(bool lockMovement)
+    {
+        if (player != null)
+        {
+            var playerController = player.GetComponent<PlayerMovement>();
+            if (playerController != null)
+            {
+                playerController.enabled = !lockMovement;
+            }
+        }
+    }
+
+    private void FreezePlayerPhysics()
+    {
+        if (player != null)
+        {
+            var rb = player.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.simulated = false;
+            }
+        }
+    }    
 }
