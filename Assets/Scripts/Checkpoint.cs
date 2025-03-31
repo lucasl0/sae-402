be1f1ce7e55fc12506bfa68df62eeeb7107ab1f2
lucasl0 <lucas2005leo@gmail.com>
2025-03-31 14:04:23 +0200

    animation du checkpoint
    animation du checkpoint

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,6 +4,7 @@ public class Checkpoint : MonoBehaviour
 {
     public BoxCollider2D bc2d;
     public Vector3Variable lastCheckpointposition;
+    public Animator animator;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -11,6 +12,7 @@ public class Checkpoint : MonoBehaviour
             PlayerSpawn playerSpawn = collision.GetComponent<PlayerSpawn>();
             if (playerSpawn != null)
             {
+                animator.SetTrigger("IsActivated");
                 playerSpawn.currentSpawnPosition = transform.position;
                 bc2d.enabled = false;
                 lastCheckpointposition.CurrentValue = transform.position;
